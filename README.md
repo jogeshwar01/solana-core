cecdc2063ac34135b8b827ec8ba826ae40159f71
Jogeshwar Singh <singhjogeshwar007@gmail.com>
2024-04-14 18:13:33 +0530

    solana wallet-adapter react

---
+++ README.md
@@ -0,0 +1,9 @@
+# Solana Core
+
+#### react-wallet
+
+![image](https://github.com/jogeshwar01/solana-core/assets/85165953/f309ad6b-7d3a-476f-9a7e-d42898ab77ca)
+
+![image](https://github.com/jogeshwar01/solana-core/assets/85165953/e8af6e94-a8f3-4b6d-b478-82fa27481780)
+
+![image](https://github.com/jogeshwar01/solana-core/assets/85165953/8af41e91-62df-40d7-ab14-15a65eac45d4)
---
+++ react-wallet/src/components/Wallet.tsx
@@ -0,0 +1,43 @@
+import { FC, useMemo } from "react";
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import { UnsafeBurnerWalletAdapter } from "@solana/wallet-adapter-wallets";
+import {
+  WalletModalProvider,
+  WalletDisconnectButton,
+  WalletMultiButton,
+} from "@solana/wallet-adapter-react-ui";
+import { clusterApiUrl } from "@solana/web3.js";
+
+import "@solana/wallet-adapter-react-ui/styles.css";
+
+export const Wallet: FC = () => {
+  // The network can be set to 'devnet', 'testnet', or 'mainnet-beta'.
+  const network = WalletAdapterNetwork.Devnet;
+
+  // You can also provide a custom RPC endpoint.
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+
+  const wallets = useMemo(
+    () => [new UnsafeBurnerWalletAdapter()],
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [network]
+  );
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>
+          <WalletMultiButton />
+          <br />
+          <br />
+          <WalletDisconnectButton />
+          {/* Your app's components go here, nested within the context providers. */}
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+};
